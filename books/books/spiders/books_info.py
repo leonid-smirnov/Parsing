97c02004eed0ad6acaed37906f6539a6469a0d0e
ls <leonid.smirnov@yahoo.com>
2022-05-25 17:42:20 +0300

    Scrapy books parser MONGO and MYSQL

--- books/books/spiders/books_info.py
+++ books/books/spiders/books_info.py
@@ -9,13 +9,18 @@ class BooksInfoSpider(CrawlSpider):
     start_urls = ['https://books.toscrape.com/']
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"), follow=True),
-        Rule(LinkExtractor(restrict_xpaths=".//article/div[@class='image_container']/a"), callback='parse_item', follow=True)
+        Rule(LinkExtractor(restrict_xpaths="//article/div[@class='image_container']/a"), callback='parse_item', follow=True),
+        #Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"), follow=True)
     )
 
     def parse_item(self, response):
         item = {}
         item['title'] = response.xpath("//div[contains(@class, 'product_main')]/h1/text()").get()
         item['price'] = response.xpath("//p[@class='price_color']/text()").get()
+        item['image'] = response.xpath("//div[@class='image_container']/a/img/@src").get()
         item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+
         return item
+
+
+
